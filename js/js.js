1db1e07289e58017a1ba38bb71a2437c98a4672f
Rojanie <rojaniebeberino0206@gmail.com>
2020-12-08 16:50:45 +0800

    finished homepage

---
+++ js/js.js
@@ -0,0 +1,49 @@
+$(function(){
+
+
+    $(window).on('scroll', function(){
+       if ($(window).scrollTop()){
+           $('nav').addClass('shrink');
+       }else{
+        $('nav').removeClass('shrink');
+       }
+    });
+
+
+
+    $(window).on('scroll', function(){
+        if ($(window).scrollTop()){
+            $('.nav-menu').addClass('fallow');
+        }else{
+         $('.nav-menu').removeClass('fallow');
+        }
+     });
+
+
+     $('.burger').on('click', function(){
+         console.log('hey====>');
+         $('.nav-menu').toggleClass('togg');
+     });
+
+     $('.sub-title').hover( function(){
+        $('.sub-title').hover(function(){
+            $('.hr').css('width', '30%');
+        },function(){
+            $('.hr').css('width', '10%')
+        });
+     });
+
+    $('.modalBtn').on('click', function() {
+        // $('.modal').css('display', 'flex');
+        var modalElements = $(this).attr ('data-modals');
+        $('#' + modalElements).toggle();
+    });
+    $('.hidden').on('click', function(){
+        $('.modal').fadeOut();
+    });
+
+    //  $('.elementItems').on('click', function() {
+    //     var elemId = $(this).attr('data-elemid');
+    //     $('#' + elemId).toggle();
+    //  });
+});
