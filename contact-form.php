69559b16cc5e169b366ee4d1131f64be5f966f1b
Rojanie <rojaniebeberino0206@gmail.com>
2020-12-09 16:47:12 +0800

    creating contact form

---
+++ contact-form.php
@@ -0,0 +1,19 @@
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['messages'];
+
+
+    $con = new mysqli('localhost', 'root', '', 'test');
+    if ($con->connect_error){
+        die('Connection Failed====>'.$con->connect_error);
+    }else{
+        $test = $con->prepare('insert into validation-form(name, email, message) value(?,?,?)');
+        $test->bind_param("sss"$name,$email,$messase);
+        $test->execute();
+        echo("Your Message is Recieved!!");
+        $test->close();
+        $con->close();
+    }
+
+?>
