a4f897999d6e9c719da62350be186dcdd94ddad1
Rojanie <rojaniebeberino0206@gmail.com>
2020-12-12 23:56:25 +0800

    Cleaning the contact form

--- contact-form.php
+++ contact-form.php
@@ -1,20 +1,43 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 <?php
-    $conn = new mysqli('localhost', 'root', '', 'sample');
 
-    $username = $_POST['name'];
+    //Connection to the dataMysql
+    $conn = new mysqli('localhost', 'root', '', 'projectrj');
+                      //localhost     name      Database Folder
+
+    //Creating Variable for the inputs in html
+    $username = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
     $email = $_POST['email'];
-    $sql = "INSERT INTO inputpost (name, email) VALUES ('$username', '$email')";
-    $select = "SELECT * FROM inputpost";
+    $phonenum = $_POST['number'];
+    $message = $_POST['message'];
 
-    if (isset($_POST['name'], $_POST['email'])){
+
+    //Query for inserting Data from inputs
+    $sql = "INSERT INTO contactform (firstname, lastname, email, number, message) VALUES ('$username','$lastname','$email','$phonenum', '$message')";
+
+    //Query for selecting data for the database.
+    //$select = "SELECT * FROM inputpost";
+
+
+    //function to Execute the program.
+    if (isset($_POST['firstname'],$_POST['lastname'], $_POST['email'], $_POST['number'] ,$_POST['message'])){
         if ($conn->query($sql) === TRUE) {
-            $result = $conn->query($select);
-            while($row = $result->fetch_assoc()){
-                echo "id: " . $row["id"]. " - Name: " . $row["name"]. " " . $row["email"]. "<br>";
-            }
+            echo "Thanks for getting in touch with us.";
         } else {
             echo "Error: " . $sql . "<br>" . $conn->error;
         }
-    }
+    }else{
 
+    }
 ?>
+<body>
+</body>
+</html>
